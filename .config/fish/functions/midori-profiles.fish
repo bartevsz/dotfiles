8c6f1b3a903d2d8ed244e79407d126a4368e690e
Mateusz Bartoszewski <mateusz.bartoszewski@outlook.com>
2026-04-28 08:44:52 +0200

    Aktualizacja konfiguracji nvim + Obsidian

--- .config/fish/functions/midori-profiles.fish
+++ .config/fish/functions/midori-profiles.fish
@@ -1,6 +1,10 @@
-for profile in ~/.config/mozilla/midori/*.*
-    if test -d $profile
-        ln -sf ~/dotfiles/midori/user.js $profile/user.js
-        echo "Zabezpieczono profil: "(basename $profile)
+function midori-profiles --description 'Automatycznie zabezpiecza wszystkie profile przeglądarki Midori utwardzonym plikiem user.js'
+    for profile in ~/.config/mozilla/midori/*.*
+        if test -d $profile
+            ln -sf ~/dotfiles/midori/user.js $profile/user.js
+            set_color green
+            echo "Zabezpieczono profil: "(basename $profile)
+            set_color normal
+        end
     end
 end
