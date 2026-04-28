7ecaa01234f40b8a40bf03cbdba8aff93e1cf97c
Mateusz Bartoszewski <mateusz.bartoszewski@outlook.com>
2026-04-28 08:27:52 +0200

    Aktualizacja konfiguracji nvim + Obsidian

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -6,9 +6,17 @@ end
 
 function fish_prompt
     set_color 00F0D8 # Ścieżka w Neon Teal
-    echo -n (prompt_pwd)
+    echo -n (prompt_pwd)' '
+    
+    # Detektor Distroboxa (Prawdziwa nazwa kontenera)
+    if test -f /run/.containerenv
+        set container_name (awk -F\" '/name=/ {print $2}' /run/.containerenv)
+        set_color yellow
+        echo -n "[📦 $container_name] "
+    end
+    
     set_color FF5E00 # Strzałka w Plazmowym Pomarańczu
-    echo -n 'λ  '
+    echo -n 'λ '
     set_color normal
 end
 
@@ -21,7 +29,12 @@ if status is-login
     ulimit -s 8192
     fish_add_path /sbin
     fish_add_path /home/mb/.spicetify
-    set -Ux fish_user_paths (npm prefix -g)/bin $fish_user_paths
+    
+    # Bezpieczne ładowanie ścieżek NPM (tylko jeśli zainstalowany)
+    if command -v npm > /dev/null
+        set -Ux fish_user_paths (npm prefix -g)/bin $fish_user_paths
+    end
+
     if test (tty) = /dev/tty1
         exec dbus-run-session Hyprland
     end
