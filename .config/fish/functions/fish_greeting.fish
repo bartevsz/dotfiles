9f87afe0d94c8a333d97380f9e10d8029c9ea8ad
bartevsz <mateusz.bartoszewski@outlook.com>
2026-06-16 17:07:52 +0200

    update dotfiles for ghostty setup

--- .config/fish/functions/fish_greeting.fish
+++ .config/fish/functions/fish_greeting.fish
@@ -1,16 +1,3 @@
 function fish_greeting
-    if test -f ~/.config/pingwiny/slowka.txt
-        # Pobieramy 3 losowe linie z pliku
-        set -l wybrane (shuf -n 3 ~/.config/pingwiny/slowka.txt)
-        
-        echo (set_color yellow)"[RAPORT KOWALSKIEGO] - Slovíčka dňa:"(set_color normal)
-        
-        # Pętla wyświetlająca każde słówko w nowej linii z wcięciem
-        for slowko in $wybrane
-            echo (set_color cyan)"  -> "(set_color normal)$slowko
-        end
-    else
-        echo (set_color red)"Błąd: Brak pliku ze słówkami w ~/.config/pingwiny/"(set_color normal)
-    end
+    echo "hello, $USER"
 end
-
--- midori/user.js
+++
@@ -1,3 +0,0 @@
-user_pref("gfx.webrender.all", true);
-user_pref("gfx.webrender.compositor", false);
-user_pref("gfx.webrender.compositor.force-enabled", false);
